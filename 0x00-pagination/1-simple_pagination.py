789124745f6c011398e97465e807b0d08fe274c8
Geobellz22 <st377126@gmail.com>
2023-10-22 21:27:03 +0000

    Task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-Copy index_range from the previous task
- and the following class into your code
+Defines class Server that paginates a database of popular baby names
 """
 import csv
 import math
@@ -12,18 +11,17 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return tuple of size two"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-    return (start_index, end_index)
+    return start_index, end_index
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
+    def dataset(self) -> List[List[str]]:
         """Cached dataset
         """
         if self.__dataset is None:
@@ -34,15 +32,17 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data
-        """
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List[str]]:
+        """Returns requested page from the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
+
         dataset = self.dataset()
         total_items = len(dataset)
+
         start_index, end_index = index_range(page, page_size)
 
         if start_index >= total_items:
             return []
-            return dataset[start_index:end_index]
+
+        return dataset[start_index:end_index]
