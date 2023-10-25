62a3115730185f0c5437b37085cde893f57cab80
Geobellz22 <st377126@gmail.com>
2023-10-25 21:54:59 +0000

    Task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
-"""FIFOCache BaseCaching system"""
+"""FIFOCache caching system"""
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """fifo cache class"""
+    """FIFO Cache class"""
     def __init__(self):
         """init method"""
         super().__init__()
 
     def put(self, key, item):
-        """Assign value for key"""
+        """Assign value to a given key"""
         if key is None or item is None:
             return
 
-
-        if len(self.cache_data) >= BaseCaching.MAX_TITLES:
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
             if key not in self.cache_data.keys():
                 f_key = next(iter(self.cache_data))
                 del self.cache_data[f_key]
                 print(f"DISCARD {f_key}")
 
-        self.cache_data(key) = item
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Get value"""
+        """Get value of a given key"""
         return self.cache_data.get(key)
