5c1b4844825081f8e3191f2b262695cfe98c954f
Geobellz22 <st377126@gmail.com>
2023-10-25 21:48:18 +0000

    Task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""FIFOCache BaseCaching system"""
+
+from base_caching import BaseCaching
+
+class FIFOCache(BaseCaching):
+    """fifo cache class"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Assign value for key"""
+        if key is None or item is None:
+            return
+        
+
+        if len(self.cache_data) >= BaseCaching.MAX_TITLES:
+            if key not in self.cache_data.keys():
+                f_key = next(iter(self.cache_data))
+                del self.cache_data[f_key]
+                print(f"DISCARD {f_key}")
+
+        self.cache_data(key) = item
+
+    def get(self, key):
+        """Get value"""
+        return self.cache_data.get(key)
