8f8831d917830a6aa0abd5579df3ad3a62a6b228
Geobellz22 <st377126@gmail.com>
2023-11-05 12:45:51 +0000

    Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 """A Basic Flask app.
 """
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Config:
@@ -21,16 +21,16 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """Retrieves the locale for a web page
+    """Retrieves the locale for a web page.
     """
-    return request.accept_languages.best_matches(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
 def get_index() -> str:
     """The home/index page.
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
