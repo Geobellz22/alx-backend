c7ce64bf99794c79be2c6162208be376491b75f9
Geobellz22 <st377126@gmail.com>
2023-11-05 12:28:07 +0000

    Task 1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-"""Basic Flask app setup"""
-
-from flask import Flask, render_template
+"""A Basic Flask app.
+"""
 from flask_babel import Babel
+from flask import Flask, render_template
 
 
-class Config(object):
-    """config class"""
+class Config:
+    """Represents a Flask Babel configuration.
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -18,11 +19,12 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@app.route("/")
-def index_page() -> str:
-    """index route"""
-    return render_template('1-index-html')
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
